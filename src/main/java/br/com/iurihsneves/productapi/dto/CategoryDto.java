55c66fb12cc2080d78cd3e46d9fd480a3a9bcc59
Iuri Hildebrando dos Santos Neves <iuri.neves@meta.com.br>
2021-12-30 22:46:24 -0300

    Starting to build the repository classes

--- src/main/java/br/com/iurihsneves/productapi/dto/CategoryDto.java
+++ src/main/java/br/com/iurihsneves/productapi/dto/CategoryDto.java
@@ -1,12 +1,17 @@
 package br.com.iurihsneves.productapi.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import br.com.iurihsneves.productapi.model.Category;
 import lombok.Data;
 
 @Data
 public class CategoryDto {
 
+    @NotNull
     private long id;
+    @NotBlank
     private String category_name;
 
     public static CategoryDto convert(Category category) {
