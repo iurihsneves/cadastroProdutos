38615c93172ff8aff98fbf2c13132c1f84797ef7
Iuri Neves <iurihsneves@gmail.com>
2022-01-03 21:00:00 -0300

    Adição da camada de serviços e complemento da ProductRepository

--- src/main/java/br/com/iurihsneves/productapi/repository/ProductRepository.java
+++ src/main/java/br/com/iurihsneves/productapi/repository/ProductRepository.java
@@ -1,6 +1,10 @@
 package br.com.iurihsneves.productapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.iurihsneves.productapi.model.Product;
@@ -8,6 +12,12 @@ import br.com.iurihsneves.productapi.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+    @Query(value = "select p "
+        + " from product p "
+        + " join category c on p.category.id = c.id "
+        + " where c.id = :categoryId ")
+    public List<Product> getProductByCategory(@Param("categoryId") long categoryId);
 
+    public Product findByProductIdentifier(String productIdentifier);
     
 }
---
+++ src/main/java/br/com/iurihsneves/productapi/service/ProductService.java
@@ -0,0 +1,72 @@
+package br.com.iurihsneves.productapi.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.iurihsneves.productapi.dto.ProductDto;
+import br.com.iurihsneves.productapi.model.Product;
+import br.com.iurihsneves.productapi.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductDto> getAll() {
+        
+        List<Product> products = productRepository.findAll();
+
+        return products
+                .stream()
+                .map(ProductDto::convert)
+                .collect(Collectors.toList());
+
+    }
+
+    public List<ProductDto> getProductByCategoryId(Long categoryId) {
+
+        List<Product> products = productRepository.getProductByCategory(categoryId);
+
+        return products
+                .stream()
+                .map(ProductDto::convert)
+                .collect(Collectors.toList());
+
+    }
+
+    public ProductDto findByProductIdentifier(String productIdentifier) {
+
+        Product product = productRepository.findByProductIdentifier(productIdentifier);
+
+        if(product != null) {
+            return ProductDto.convert(product);
+        }
+
+        return null;
+
+    }
+
+    public ProductDto save(ProductDto productDto) {
+
+        Product product = productRepository.save(Product.convert(productDto));
+        
+        return ProductDto.convert(product);
+
+    }
+
+    public void delete(long productId) {
+
+        Optional<Product> product = productRepository.findById(productId);
+
+        if(product.isPresent()) {
+            productRepository.delete(product.get());
+        }
+
+    }
+    
+}
